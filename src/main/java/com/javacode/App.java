1eabd3aa4a0918b6119f6ba20c790da048207bf4
Igar Sushko <igar.sushko@gmail.com>
2012-11-28 06:50:13 +0800

    Добавлена базовая настройка для logback.

--- src/main/java/com/javacode/App.java
+++ src/main/java/com/javacode/App.java
@@ -1,9 +1,14 @@
 package com.javacode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App
 {
+    static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args)
     {
-        System.out.println("JavaCode");
+        logger.info("JavaCode");
     }
 }
